b2860fd63ebd0f0a61125cf0e89d16ed1cf2fae6
[alvinz97] <it18211306@my.sliit.lk>
2020-03-29 16:33:43 +0530

    Update

---
+++ app/src/main/java/com/example/labsheet05/BackgroundServices.java
@@ -0,0 +1,38 @@
+package com.example.labsheet05;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class BackgroundServices extends IntentService {
+
+    public BackgroundServices() {
+        super("BackgroundServices");
+    }
+
+    public static void startAction(Context context){
+        Intent intent = new Intent(context, BackgroundServices.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            for (int i = 0; i < 5; i++) {
+                Intent localBroadcastIntent = new Intent(MainActivity.BROADCAST_ACTION);
+                localBroadcastIntent.putExtra("value" , "Broadcast " + (i + 1));
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                sendBroadcast(localBroadcastIntent);
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/labsheet05/MainActivity.java
+++ app/src/main/java/com/example/labsheet05/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.labsheet05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String BROADCAST_ACTION = "";
+    private Button broadcastBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
